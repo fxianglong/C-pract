65d76a0d711094f8537ccb608afe355e28a4a811
Jack feng <48426950+fxianglong@users.noreply.github.com>
2019-12-08 23:09:37 +0800

    11.8练习
    11.8练习

--- 源.cpp
+++ 源.cpp
@@ -1,111 +1,61 @@
 //#include<iostream>
+//#include<vector>
+//#include<math.h>
 //#include<string>
 //#include<algorithm>
 //using namespace std;
-//int main()
-//{
-//	string s;
-//	cin >> s;
-//	reverse(s.begin(),s.end());
-//	cout << s << endl;
-//	return 0;
-//}
-
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//int getcommentlength(string s1, string s2)
-//{
-//	if (s1 > s2)
-//		swap(s1, s2);
-//	int count = 0;
-//	for (int i = 0; i < s1.size(); i++)
+//int getFirstUnFormedNum(vector<int> arr, int len) {
+//	if (len == 1) return arr[0] + 1;
+//	int min = arr[0], max = arr[0];
+//	int i = 0, j = 0;
+//	for (i = 1; i<len; i++)
 //	{
-//		int ret = 0;
-//		int j = 0;
-//		while (j<s2.size())
-//		{
-//			int m = i;
-//			j = s2.find(s1[m], j);   //在s2中找s1的字母
-//			while (s1[m] == s2[j] && m<s1.size())   //如果找到，ret计数++，并继续查找
-//			{
-//				ret++;
-//				m++; j++;
-//			}
-//			if (ret>count)   //比较ret 与count，取较大的值
-//				count = ret;
-//			ret = 0;
-//		}
+//		max += arr[i];
+//		min = (min<arr[i]) ? min : arr[i];
 //	}
-//	return count;
-//
+//	int res[100001] = { 0 };
+//	res[0] = 1;
+//	i = 0;
+//	while (i<len)
+//	{
+//		for (j = max; j >= 0; j--)
+//		if (res[j])
+//			res[j + arr[i]] = 1;
+//		i++;
+//	}
+//	i = min;
+//	while (i <= max&&res[i]) i++;
+//	return i;
 //}
 //int main()
 //{
-//	string s1, s2;
-//	cin >> s1;
-//	cin >> s2;
-//
-//	cout << getcommentlength(s1, s2) << endl;
-//	return 0;
-//}
-
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//const int Tabsize = 256;
-//int hashtab[Tabsize];
-//int main(){
-//	string str;
-//	while (cin >> str){
-//		bool flg = false;
-//		for (int i = 0; i<Tabsize; ++i) {
-//			hashtab[i] = 0;
-//		}
-//		for (int i = 0; i<str.size(); ++i){
-//			hashtab[str[i]]++;
-//		}
-//		for (int i = 0; !flg && i<str.size(); ++i){
-//			if (hashtab[str[i]] == 1){
-//				cout << str[i] << endl;
-//				flg = true;
-//				break;
-//			}
-//		}
-//		if (!flg)
-//			cout << '-1' << endl;
+//	vector<int> s;
+//	int len = 0; cin >> len; 
+//	int l = 0; int a[1000];
+//	for (int i = 0; i < len; i++)
+//	{
+//		cin >> a[i];
+//		s.push_back(a[i]);
 //	}
+//	cout << getFirstUnFormedNum(s, len) << endl;
 //	return 0;
 //}
 
-
-#include <iostream>
-#include "stdio.h"
-#include <vector>
+#include<iostream>
 using namespace std;
-int gcd(int m, int n)
-{
-	if (n == 0)
-		return m;
-
-	int temp = m%n;
-	m = n;
-	n = temp;
-	return gcd(m, n);
-}
 int main()
 {
-	int n, ans;
-	while (cin >> n >> ans)
+	int n;
+	while (scanf("%d", &n), n)
 	{
-		while (n>0)
+		int count = 0;
+		while (n>1)
 		{
-			int cur;
-			cin >> cur;
-			ans = ans + ((ans<cur) ? gcd(cur, ans) : cur);
-			n--;
+			n = n / 3 + 1 - (n % 3 ^ 3) / 3;
+			count++;
 		}
-		cout << ans << endl;
+		printf("%d\n", count);
 	}
+	return 0;
 	return 0;
 }
