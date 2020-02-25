28068306fa772fd772273a595e9f9cbe74992641
fxianglong <1244190336@qq.com>
2020-02-25 12:56:49 +0800

    字符串和字符串函数

---
+++ 练习/C和指针/字符串函数/字符串函数/模拟实现strcpy.cpp
@@ -0,0 +1,23 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<assert.h>
+//char* Mystrcpy(char* desc, const char* src)
+//{
+//	assert(desc != NULL);
+//	assert(src != NULL);
+//	char* ret = desc;
+//	while (*src != '\0')
+//	{
+//		*desc++ = *src++;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char res[] = "juhygtr";
+//	char ans[sizeof(res)];
+//	Mystrcpy(ans, res);
+//	printf("%s", ans);
+//	system("pause");
+//	return 0;
+//}
---
+++ 练习/C和指针/字符串函数/字符串函数/模拟实现strlen.cpp
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//size_t Mystrlen(const char* str)
+//{
+//	int count = 0;
+//	while (*str++ != '\0')
+//	{
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char s[] = "jhygtdxdcfvg";
+//	printf("%d", Mystrlen(s));
+//	system("pause");
+//	return 0;
+//}
