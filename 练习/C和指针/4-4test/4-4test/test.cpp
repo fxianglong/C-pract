3d6db50373d8c37f8c5fde837cdeb8b5189d4e09
fxianglong <1244190336@qq.com>
2020-04-04 21:37:05 +0800

    C-文件操作；C++-类和对象

---
+++ 练习/C和指针/4-4test/4-4test/test.cpp
@@ -0,0 +1,475 @@
+﻿//#include <stdio.h>
+//
+//int main()
+//{
+//	FILE * pFile;
+//	pFile = fopen("example.txt", "wb");
+//	fputs("This is an apple.", pFile);
+//	fseek(pFile, 2, SEEK_END);
+//	fputs(" sam", pFile);
+//	fclose(pFile);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	FILE * pFile;
+//	long size;
+//
+//	pFile = fopen("myfile.txt", "rb");
+//	if (pFile == NULL) perror("Error opening file");
+//	else
+//	{
+//		fseek(pFile, 0, SEEK_END); // non-portable
+//		size = ftell(pFile);
+//		fclose(pFile);
+//		printf("Size of myfile.txt: %ld bytes.\n", size);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n;
+//	FILE * pFile;
+//	char buffer[27];
+//
+//	pFile = fopen("myfile.txt", "w+");
+//	for (n = 'A'; n <= 'Z'; n++)
+//		fputc(n, pFile);
+//	rewind(pFile);
+//	fread(buffer, 1, 26, pFile);
+//	fclose(pFile);
+//	buffer[26] = '\0';
+//	puts(buffer);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(void)
+//{
+//	int c; // 注意：int，非char，要求处理EOF
+//	FILE* fp = fopen("test.txt", "r");
+//	if (!fp) {
+//		perror("File opening failed");
+//		return EXIT_FAILURE;
+//	}
+//	//fgetc 当读取失败的时候或者遇到文件结束的时候，都会返回EOF
+//	while ((c = fgetc(fp)) != EOF) // 标准C I/O读取文件循环
+//	{
+//		putchar(c);
+//	}
+//	//判断是什么原因结束的
+//	if (ferror(fp))
+//		puts("I/O error when reading");
+//	else if (feof(fp))
+//		puts("End of file reached successfully");
+//
+//	fclose(fp);
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//enum { SIZE = 5 };
+//int main(void)
+//{
+//	double a[SIZE] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+//	double b = 0.0;
+//	size_t ret_code = 0;
+//	FILE *fp = fopen("test.bin", "wb"); // 必须用二进制模式
+//	fwrite(a, sizeof(*a), SIZE, fp); // 写 double 的数组
+//	fclose(fp);
+//
+//	fp = fopen("test.bin", "rb");
+//	// 读 double 的数组
+//	while ((ret_code = fread(&b, sizeof(double), 1, fp)) >= 1)
+//	{
+//		printf("%lf\n", b);
+//	}
+//	if (feof(fp))
+//		printf("Error reading test.bin: unexpected end of file\n");
+//	else if (ferror(fp)) {
+//		perror("Error reading test.bin");
+//	}
+//	fclose(fp);
+//	system("pause");
+//	fp = NULL;
+//}
+
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	printf("file:%s line:%d\n%s\n%s\n", __FILE__, __LINE__,__DATE__ ,__TIME__ );
+//	//printf("%d",__STDC__);
+//	system("pause");
+//	return 0;
+//}
+//#define MAX 1000
+//#define reg register          //为 register这个关键字，创建一个简短的名字
+//#define do_forever for(;;)     //用更形象的符号来替换一种实现
+//#define CASE break;case        //在写case语句的时候自动把 break写上。
+//// 如果定义的 stuff过长，可以分成几行写，除了最后一行外，每行的后面都加一个反斜杠(续行符)。
+//#define DEBUG_PRINT printf("file:%s\tline:%d\t date:%s\ttime:%s\n", __FILE__, __LINE__, __DATE__, __TIME__)
+//
+//int main()
+//{
+//	DEBUG_PRINT;
+//	system("pause");
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//
+//int& Add(int a, int b)
+//{
+//	int c = a + b;
+//	return c;
+//}
+//
+//int main()
+//{
+//	int& ret = Add(1, 2);
+//	Add(3, 4);
+//	cout << "Add(1, 2) is :" << ret << endl;
+//	system("pause");
+//	return 0;
+//}
+
+//#include<iostream>
+//#include <time.h>
+//using namespace std;
+//
+//struct A
+//{
+//	int a[10000];
+//};
+//
+//
+//void TestFunc1(A a)
+//{}
+//
+//void TestFunc2(A& a)
+//{}
+//
+//void TestRefAndValue()
+//{
+//	A a;
+//
+//	// 以值作为函数参数
+//	size_t begin1 = clock();
+//	for (size_t i = 0; i < 10000; ++i)
+//		TestFunc1(a);
+//	size_t end1 = clock();
+//
+//	// 以引用作为函数参数
+//	size_t begin2 = clock();
+//	for (size_t i = 0; i < 10000; ++i)
+//		TestFunc2(a);
+//	size_t end2 = clock();
+//
+//	// 分别计算两个函数运行结束后的时间
+//	cout << "TestFunc1(int*)-time:" << end1 - begin1 << endl;
+//	cout << "TestFunc2(int&)-time:" << end2 - begin2 << endl;
+//}
+//
+//// 运行多次，检测值和引用在传参方面的效率区别
+//int main()
+//{
+//	for (int i = 0; i < 10; ++i)
+//	{
+//		TestRefAndValue();
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+#include<iostream>
+using namespace std;
+//class A
+//{
+//public:
+//	void PrintA()
+//	{
+//		cout << _a << endl;
+//	}
+//
+//	void Show()
+//	{
+//		cout << "Show()" << endl;
+//	}
+//private:
+//	int _a;
+//};
+//
+//int main()
+//{
+//	A* p=NULL;
+//	p->PrintA();
+//	p->Show();
+//}
+
+//class String
+//{
+//public:
+//	String(const char* str = "jack")
+//	{
+//		_str = (char*)malloc(strlen(str) + 1);
+//		strcpy(_str, str);
+//	}
+//
+//	~String()
+//	{
+//		cout << "~String()" << endl;
+//		free(_str);
+//	}
+//private:
+//	char* _str;
+//};
+//
+//int main()
+//{
+//	String s1("hello");
+//	String s2(s1);
+//}
+
+//
+//class String
+//{
+//public:
+//	String(const char* str = "jack")
+//	{
+//		_str = (char*)malloc(strlen(str) + 1);
+//		strcpy(_str, str);
+//	}
+//
+//	~String()
+//	{
+//		cout << "~String()" << endl;
+//		free(_str);
+//	}
+//private:
+//	char* _str;
+//};
+//
+//int main()
+//{
+//	String s1("hello");
+//	String s2("world");
+//
+//	s1 = s2;
+//}
+
+
+//class Date
+//{
+//public:
+//	Date(int year)
+//		:_year(year)
+//	{}
+//	explicit Date(int year)
+//		:_year(year)
+//	{}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//void TestDate()
+//{
+//	Date d1(2018);
+//
+//	// 用一个整形变量给日期类型对象赋值
+//	// 实际编译器背后会用2019构造一个无名对象，最后用无名对象给d1对象进行赋值
+//	d1 = 2019;
+//}
+//int main()
+//{
+//	TestDate();
+//	return 0;
+//}
+
+
+//class A
+//{
+//public:
+//	A() { ++_scount; }
+//	A(const A& t) { ++_scount; }
+//	static int GetACount() { return _scount; }
+//private:
+//	static int _scount;
+//};
+//
+//int A::_scount = 0;
+//
+//void TestA()
+//{
+//	cout << A::GetACount() << endl;
+//	A a1, a2;
+//	A a3(a1);
+//	cout << A::GetACount() << endl;
+//}
+//int main()
+//{
+//	TestA();
+//	return 0;
+//}
+
+//class Date
+//{
+//	friend ostream& operator<<(ostream& _cout, const Date& d);
+//	friend istream& operator>>(istream& _cin, const Date& d);
+//public:
+//	Date(int year, int month, int day)
+//		:	_year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//ostream& operator<<(ostream& _cout, const Date& d)
+//{
+//	_cout << d._year << "-" << d._month << "-" << d._day;
+//
+//	return _cout;
+//}
+//
+//istream& operator>>(istream& _cin, const Date& d)
+//{
+//	_cin >> d._year;
+//	_cin >> d._month;
+//	_cin >> d._day;
+//
+//	return _cin;
+//}
+//
+//int main()
+//{
+//	Date d;
+//	cin >> d;
+//	cout << d << endl;
+//	return 0;
+//}
+
+
+//class Date;   // 前置声明
+//class Time
+//{
+//	friend class Date;   // 声明日期类为时间类的友元类，则在日期类中就直接访问Time类中的私有成员变
+//public:
+//	Time(int hour, int minute, int second)
+//		: _hour(hour)
+//		, _minute(minute)
+//		, _second(second)
+//	{}
+//
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//
+//	void SetTimeOfDate(int hour, int minute, int second)
+//	{
+//		// 直接访问时间类私有的成员变量
+//		_t._hour = hour;
+//		_t._minute = minute;
+//		_t.second = second;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//	Time _t;
+//};
+
+
+//class A
+//{
+//private:
+//	static int k;
+//	int h;
+//public:
+//	class B
+//	{
+//	public:
+//		void foo(const A& a)
+//		{
+//			cout << k << endl;//OK
+//			cout << a.h << endl;//OK
+//		}
+//	};
+//};
+//int A::k = 1;
+//
+//int main()
+//{
+//	A::B b;
+//	b.foo(A());
+//
+//	return 0;
+//}
+
+
+#include<iostream>
+using namespace std;
+
+	int month[13] = { 0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
+	bool Isleap(int _y){
+		if ((_y % 4 == 0 && _y % 100 != 0) || (_y % 400 == 0))//（被4整除||被100整除）&&不能被400整除
+			return true;
+		return false;
+	}
+	int Description(int year,int Month,int Day)
+	{
+		if (Isleap(year))
+		{
+			if (Month>2)
+			{
+				return (month[Month] + Day + 1);
+			}
+			else
+				return (month[Month] + Day);
+		}
+		else
+			return (month[Month] + Day);
+	}
+
+int main()
+{
+	int y, m, d;
+	cin >> y >> m >> d;
+
+	if (y > 0 && m >= 1 && m <= 13 && d > 0 && d < 32)
+	{
+	int ret = Description(y, m, d);
+	cout << ret << endl;
+		return 0;
+	}
+	else
+		return -1;
+}
