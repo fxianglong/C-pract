d0c39ab62733cf4ef9283071b6686d5e271b5bdc
fxianglong <1244190336@qq.com>
2020-04-22 23:05:19 +0800

    4-22test

---
+++ 练习/C和指针/AVL树_红黑树/AVL树_红黑树/AVLTree.h
@@ -0,0 +1,32 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//template<class T>
+//class AVLTree
+//{
+//	struct AVLTreeNode
+//	{
+//		AVLTreeNode(const T& data)
+//		:_pLeft(nullptr)
+//		, _pRight(nullptr)
+//		, _pParent(nullptr)
+//		, _data(data)
+//		, _bf(0)
+//		{}
+//		AVLTreeNode<T>* _pLeft;//该节点左子树
+//		AVLTreeNode<T>* _pRight;//该节点右子树
+//		AVLTreeNode<T>* _pParent;//该节点父母节点
+//		T _data;
+//		int _bf;//平衡因子
+//	};
+//public:
+//	AVLTree();
+//	~AVLTree();
+//	bool Insert(const T& data)
+//	{
+//
+//	}
+//private:
+//	AVLTreeNode _Root;
+//};
